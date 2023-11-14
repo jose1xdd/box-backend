29fceed5d4e7e4afff6dd39329f620e5b005f58c
JoseJ <alvarezreyes246@gmail.com>
2023-11-14 16:28:15 -0500

    Se ha creado el metodo para actualizar el club de un usuario

--- src/database/models/static/webToken.ts
+++ src/database/models/static/webToken.ts
@@ -2,5 +2,8 @@ import { Model } from 'mongoose';
 import { IWebTokenDocument } from '../../../types/webToken';
 
 export class webTokenModel extends Model<IWebTokenDocument> {
-
+	static async getIdUser(token: string){
+		const result = await this.findOne({ token: token });
+		return result.userId;
+	}
 }
