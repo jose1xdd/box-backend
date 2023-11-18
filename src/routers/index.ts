934c2c80ab80a979b3c8d7a7bdde16c209e20702
JoseJ <alvarezreyes246@gmail.com>
2023-11-18 15:12:36 -0500

    Se ha completado el crud inicial de criterio de evaluacion, Crear, eliminar y listar

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,9 +3,11 @@ import { userRouter } from './user';
 import { loginRouter } from './login';
 import { clubsRouter } from './club';
 import { weightCategoryRouter } from './weightCategory';
+import { testCriteryRouter } from './testCritery';
 
 export const mainRouter = express.Router({ mergeParams: true });
 mainRouter.use('/users', userRouter);
 mainRouter.use('/login', loginRouter);
 mainRouter.use('/club', clubsRouter);
 mainRouter.use('/weightCategory', weightCategoryRouter);
+mainRouter.use('/testCritery', testCriteryRouter);
