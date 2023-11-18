45ff8763a1f2ffd1166b353b446b340e9fcec32e
JoseJ <alvarezreyes246@gmail.com>
2023-11-18 15:12:36 -0500

    Se ha completado el crud inicial de criterio de evaluacion, Crear, eliminar y listar

---
+++ src/routers/testCritery.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { bodyValidator, queryValidator } from '../middlewares/validator';
+import * as yup from 'yup';
+import { checkSession } from '../middlewares/checkSession';
+import { checkAuth } from '../middlewares/checkAuth';
+import { testCriteryController } from '../controllers/testCritery';
+
+export const testCriteryRouter = Router({ mergeParams: true });
+
+//create a critery
+testCriteryRouter.post('/', bodyValidator(yup.object().shape({
+	name: yup.string().required(),
+}).noUnknown(true)), checkSession, checkAuth(['Admin']), testCriteryController.createCritery);
+
+// Delete a critery
+testCriteryRouter.delete('/', queryValidator(yup.object().shape({
+	criteryId: yup.string().required(),
+}).noUnknown(true)), checkSession, checkAuth(['Admin']), testCriteryController.deleteCritery);
+
+//get critery list
+testCriteryRouter.get('/', queryValidator(yup.object().shape({
+	limit: yup.string(),
+}).noUnknown(true)), checkSession, checkAuth([]), testCriteryController.getCriteryList);
