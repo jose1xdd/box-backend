f926fa6c2743dbcb49f5fccc81c470cf3c8e7a87
JoseJ <alvarezreyes246@gmail.com>
2023-11-22 11:56:22 -0500

    Se ha creado la estructura de ranking en usuario

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 import { IphysicalTest } from './physicalTest';
 import { IweightCategory } from './weightCategory';
+export interface Iranking {
+    win: number,
+    lose: number,
+    draw:number,
+}
 export interface Irole {
     name: string,
     permissions: string
@@ -18,6 +23,7 @@ export interface Iuser {
     password : string,
     role : string | Irole,
     photo? : string,
+    ranking?: Iranking,
     weightCategory? : IweightCategory,
     physicalTest? : IphysicalTest[]
 }
