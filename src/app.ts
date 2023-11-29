cd768ec81406650c95d7f9e156700be7f77a1439
Jose David Castillo <jose1xdd@gmail.com>
2023-11-29 10:55:27 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,9 @@ import cors from 'cors';
 configDotenv();
 connection();
 const app = express();
-app.use(cors());
+app.use(cors({
+	origin: 'http://181.33.201.190:3000'
+}));
 app.use(express.json());
 app.listen(8020, ()=>{
 	logger.info('Servidor Corriendo en el Puerto 8020');
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -2,6 +2,7 @@ import multer from 'multer';
 import { storages } from './firebase';
 import path from 'path';
 import fs from 'fs';
+import { logger } from '../logger/winston';
 
 export const storageBucket = storages.bucket();
 
@@ -19,4 +20,32 @@ const storage = multer.diskStorage({
 		cb(null, file.originalname);
 	},
 });
+export function emptyUploadFolder() {
+	const folderPath = `${__dirname}/../uploads`;
+	fs.readdir(folderPath, (err, filesD) => {
+		filesD.forEach((file) => {
+			const filePath = path.join(folderPath, file);
+			fs.unlink(filePath, () => {});
+		});
+	});
+}
+
+export async function saveUserImages(file, userId) {
+	const extension = path.extname(file.originalname);
+	const nombreAutoGenerado = `${file.fieldname}-${Date.now()}`;
+	const remoteFilePath = `game-field/${userId}/${nombreAutoGenerado}${extension}`;
+
+	const storageFile = storageBucket.file(remoteFilePath);
+	const blobStream = storageFile.createWriteStream({
+		metadata: {
+			contentType: file.mimetype, // Establece el tipo de contenido de la imagen
+		},
+	});
+
+	await blobStream.on('finish', () => {
+		logger.info('image uploaded');
+	});
+	const readStream = fs.createReadStream(file.path);
+	readStream.pipe(blobStream);
+}
 export const upload = multer({ storage });
