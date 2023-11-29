3ac33b7b8b59560f3a86d03e4d9f66eb5bfdd43e
Jose David Castillo <jose1xdd@gmail.com>
2023-11-29 10:24:34 -0500

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import express from 'express';
 import { connection } from './database/connection';
 import { logger } from './logger/winston';
 import { mainRouter } from './routers';
-
+import cors from 'cors';
 configDotenv();
 connection();
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.listen(8020, ()=>{
 	logger.info('Servidor Corriendo en el Puerto 8020');
