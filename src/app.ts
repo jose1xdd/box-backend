8c456cdec9c724a579426e7ef3628a5d55ffc330
Jose David Castillo <jose1xdd@gmail.com>
2023-11-29 11:02:00 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,7 @@ import cors from 'cors';
 configDotenv();
 connection();
 const app = express();
-app.use(cors({
-	origin: 'http://181.33.201.190:3000'
-}));
+app.options('*', cors());
 app.use(express.json());
 app.listen(8020, ()=>{
 	logger.info('Servidor Corriendo en el Puerto 8020');
